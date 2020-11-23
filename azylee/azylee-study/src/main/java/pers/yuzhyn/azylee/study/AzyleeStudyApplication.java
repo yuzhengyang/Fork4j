e60d9cf465a75ccfdb4966f6a77d31a18c1a43a3
yuzhengyang <inc@live.cn>
2020-11-23 22:55:26 +0800

    增加设计模式package

---
+++ azylee/azylee-study/src/main/java/pers/yuzhyn/azylee/study/AzyleeStudyApplication.java
@@ -0,0 +1,13 @@
+package pers.yuzhyn.azylee.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzyleeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzyleeStudyApplication.class, args);
+    }
+
+}
---
+++ azylee/azylee-study/src/test/java/pers/yuzhyn/azylee/study/AzyleeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.yuzhyn.azylee.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzyleeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
