ea2a40146711991cf99c6a8545e8606545a06afd
yuzhengyang <inc@live.cn>
2021-01-06 22:44:06 +0800

    控制台输出日志

---
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/logs/Log.java
@@ -0,0 +1,19 @@
+package pers.yuzhyn.azylee.core.logs;
+
+public class Log {
+    public static void v(String s) {
+        System.out.println(s);
+    }
+
+    public static void i(String s) {
+        System.out.println(s);
+    }
+
+    public static void w(String s) {
+        System.out.println(s);
+    }
+
+    public static void e(String s) {
+        System.out.println(s);
+    }
+}
