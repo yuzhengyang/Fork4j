76aae6ba94ae1df74a4f774047f4d823ad54ae3d
yuzhengyang <inc@live.cn>
2021-01-06 22:21:34 +0800

    uuid注释测试内容

--- azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/uuids/UuidTool.java
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/uuids/UuidTool.java
@@ -18,8 +18,8 @@ public class UuidTool {
         return s;
     }
 
-    public static void main(String[] args) {
-        System.out.println(get());
-        System.out.println(getShort());
-    }
+//    public static void main(String[] args) {
+//        System.out.println(get());
+//        System.out.println(getShort());
+//    }
 }
