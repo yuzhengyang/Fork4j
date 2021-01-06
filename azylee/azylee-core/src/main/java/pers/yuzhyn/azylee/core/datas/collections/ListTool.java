339b4e0c69c248e54c718d5fd6b9b655fda95efd
yuzhengyang <inc@live.cn>
2021-01-06 22:20:17 +0800

    数组列表工具

--- azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/collections/ListTool.java
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/collections/ListTool.java
@@ -1,6 +1,7 @@
 package pers.yuzhyn.azylee.core.datas.collections;
 
 import java.util.Collection;
+import java.util.List;
 
 public class ListTool {
 
@@ -14,5 +15,16 @@ public class ListTool {
 
     public static boolean ok(byte[] bytes) {
         return bytes != null && bytes.length > 0;
+    }
+
+    public static <T> boolean ok(T[] array) {
+        return array != null && array.length > 0;
+    }
+
+    public static <T> T get(List<T> collection, int index, T defaultValue) {
+        if (ListTool.ok(collection) && collection.size() > index) {
+            return collection.get(index);
+        }
+        return defaultValue;
     }
 }
