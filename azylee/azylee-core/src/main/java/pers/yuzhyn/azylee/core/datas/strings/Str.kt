fd0b9c24b44a829cfce12759c9b87f0505327050
yuzhengyang <inc@live.cn>
2021-01-06 22:20:36 +0800

    字符串工具及kotlin实验

---
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/strings/Str.kt
@@ -0,0 +1,9 @@
+package pers.yuzhyn.azylee.core.datas.strings
+
+class Str {
+    companion object {
+        fun ok(s: String): Boolean {
+            return StringTool.ok(s);
+        }
+    }
+}
---
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/strings/StringConst.java
@@ -0,0 +1,5 @@
+package pers.yuzhyn.azylee.core.datas.strings;
+
+public class StringConst {
+    public static final String NEWLINE = System.getProperty("line.separator");
+}
