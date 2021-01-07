773a9159ae0151ab782467a7e0786e015ad83b1e
yuzhengyang <inc@live.cn>
2021-01-07 20:36:13 +0800

    控制台日志输出

--- azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/logs/Log.java
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/logs/Log.java
@@ -2,18 +2,18 @@ package pers.yuzhyn.azylee.core.logs;
 
 public class Log {
     public static void v(String s) {
-        System.out.println(s);
+        System.out.format("\33[32;2m%s%n",s);
     }
 
     public static void i(String s) {
-        System.out.println(s);
+        System.out.format("\33[36;2m%s%n",s);
     }
 
     public static void w(String s) {
-        System.out.println(s);
+        System.out.format("\33[33;2m%s%n",s);
     }
 
     public static void e(String s) {
-        System.out.println(s);
+        System.out.format("\33[31;2m%s%n",s);
     }
 }
---
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/systems/souts/PrintColorTool.java
@@ -0,0 +1,22 @@
+package pers.yuzhyn.azylee.core.systems.souts;
+
+import java.util.Random;
+
+public class PrintColorTool {
+
+    public static void main(String[] args) {
+        // 背景颜色代号(41-46)
+        // 前景色代号(31-36)
+        //前景色代号和背景色代号可选，就是或可以写，也可以不写
+        // 数字+m：1加粗；3斜体；4下划线
+        // 格式：System.out.println("\33[前景色代号;背景色代号;数字m");
+        Random backgroundRandom = new Random();
+        Random fontRandom = new Random();
+        for (int i = 1; i <= 50; i++) {
+            int font = fontRandom.nextInt(6) + 31;
+            int background = backgroundRandom.nextInt(6) + 41;
+//            System.out.format("前景色是%d,背景色是%d------\33[%d;%d;4m我是博主%n", font, background, font, background);
+            System.out.format("前景色是%d,背景色是%d------\33[%d;2m我是博主%n", font, background, font);
+        }
+    }
+}
