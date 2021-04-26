2d8b330b047bd0af61465a09c98c478b1a0e939c
于正洋 <yuzhyn@163.com>
2021-04-26 14:50:22 +0800

    sha256工具

---
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/encrypts/Sha256Tool.java
@@ -0,0 +1,33 @@
+package pers.yuzhyn.azylee.core.datas.encrypts;
+
+import pers.yuzhyn.azylee.core.logs.Alog;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256Tool {
+
+    public static String encrypt(String s) {
+        return encrypt(s.getBytes());
+    }
+
+    public static String encrypt(byte[] data) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(data);
+            encodeStr = HexTool.toHex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+
+    public static void main(String[] args) {
+        String text = "春风不度玉门关";
+        Alog.i(text);
+        Alog.i(Sha256Tool.encrypt(text));
+    }
+}
