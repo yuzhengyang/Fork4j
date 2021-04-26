85eb7e6fa112efbaa55f937782dcfed8878feb0a
于正洋 <yuzhyn@163.com>
2021-04-26 17:50:58 +0800

    移除逐位混合

--- azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/encrypts/MixdeTool.java
+++ azylee/azylee-core/src/main/java/pers/yuzhyn/azylee/core/datas/encrypts/MixdeTool.java
@@ -27,25 +27,8 @@ public class MixdeTool {
         return Md5Tool.encrypt(rs) + Md5Tool.encryptTwice(rs);
     }
 
-    /**
-     * 逐个md5编码并最终混合拼接
-     * 说明：循环临时编码较大
-     * 长度：64位
-     *
-     * @param s
-     * @return
-     */
-    public static String md5BigMix(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            String _m = Md5Tool.encrypt(String.valueOf(c));
-            stringBuilder.append(_m);
-        }
-        return Md5Tool.encrypt(stringBuilder.toString()) + Md5Tool.encryptTwice(stringBuilder.toString());
-    }
-
     public static void main(String[] args) {
         Alog.i(MixdeTool.md5Mix("胡天八月即飞雪"));
-        Alog.i(MixdeTool.md5BigMix("胡天八月即飞雪"));
+        Alog.i(MixdeTool.md5Mix("张三", "123456"));
     }
 }
