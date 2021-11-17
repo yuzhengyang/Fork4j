4aebfeeb12dc2689637d07bda6bf8acc76f811fc
于正洋 <yuzhyn@163.com>
2021-11-17 18:47:49 +0800

    字符串处理等工具

--- azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/strings/StringTool.java
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/strings/StringTool.java
@@ -1,5 +1,6 @@
 package com.yuzhyn.azylee.core.datas.strings;
 
+import com.yuzhyn.azylee.core.datas.collections.ListTool;
 import com.yuzhyn.azylee.core.logs.Alog;
 
 public class StringTool {
@@ -39,6 +40,139 @@ public class StringTool {
         return stringBuilder.toString();
     }
 
+    /**
+     * 开头字符计数
+     * 如：开头有几个空格
+     *
+     * @param s
+     * @param ch
+     * @return
+     */
+    public static int startCharSize(String s, char ch) {
+        int size = 0;
+        for (char c : s.toCharArray()) {
+            if (c == ch) {
+                size++;
+            } else {
+                return size;
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 缩进空格
+     *
+     * @param s
+     * @param size
+     * @return
+     */
+    public static String retractSpace(String s, int size) {
+        if (ok(s)) {
+            int startSpaceSize = startCharSize(s, ' ');
+            if (startSpaceSize > 0 && startSpaceSize >= size) {
+                return s.substring(size);
+            } else {
+                return s.substring(startSpaceSize);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * 字符串分行
+     *
+     * @param s
+     * @return
+     */
+    public static String[] splitLine(String s) {
+        if (ok(s)) {
+            String[] lines = s.split("\\r?\\n");
+            return lines;
+        }
+        return new String[]{""};
+    }
+
+    /**
+     * 字符串拆分
+     * 按照限制个数拆分，超出个数后，会使用合并连接符合并到最后一个字符串里
+     *
+     * @param s             待拆分字符串
+     * @param regex         拆分规则
+     * @param size          拆分个数限制
+     * @param combineString 超过限制的合并连接符
+     * @param filterSpace   过滤空格内容
+     * @return
+     */
+    public static String[] splitLine(String s, String regex, int size, String combineString, boolean filterSpace) {
+        String[] result = new String[size];
+        if (ok(s)) {
+            int pos = 0;
+            String[] lines = s.split(regex);
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = lines[i].trim();
+                boolean todo = true;
+                if (filterSpace) {
+                    todo = ok(lines[i]);
+                }
+
+                if (todo) {
+                    if (ok(result[pos])) {
+                        result[pos] += combineString;
+                    } else {
+                        result[pos] = "";
+                    }
+                    result[pos] += lines[i];
+                    pos++;
+                    if (pos >= size) pos = size - 1;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 字符串数组合并
+     *
+     * @param lines
+     * @param joinString
+     * @return
+     */
+    public static String combineArray(String[] lines, String joinString) {
+        if (ListTool.ok(lines)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < lines.length; i++) {
+                stringBuilder.append(lines[i]);
+                if (i < lines.length - 1) {
+                    stringBuilder.append(joinString);
+                }
+            }
+            return stringBuilder.toString();
+        }
+        return "";
+    }
+
+    /**
+     * 缩进空格，按分行自动缩进，最后再组装为字符串
+     *
+     * @param s
+     * @return
+     */
+    public static String retractSpaceArrayAuto(String s) {
+        int spaceSize = Integer.MAX_VALUE;
+        String[] lines = splitLine(s);
+        for (String line : lines) {
+            if (ok(line)) {
+                int size = startCharSize(line, ' ');
+                if (size < spaceSize) spaceSize = size;
+            }
+        }
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = retractSpace(lines[i], spaceSize);
+        }
+        return combineArray(lines, StringConst.NEWLINE);
+    }
+
     public static void main(String[] args) {
         String a = "1";
         String b = null;
@@ -52,6 +186,6 @@ public class StringTool {
         String x = "123";
         String y = "null";
 
-        Alog.i(StringTool.ok(x,y));
+        Alog.i(StringTool.ok(x, y));
     }
 }
