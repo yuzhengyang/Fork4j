ca96db51cb7d1a8ba1f897c1c687ca6694b80aed
于正洋 <yuzhyn@163.com>
2021-11-18 19:00:35 +0800

    解压缩和文件搜索工具

--- azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/ios/files/FileFindTool.java
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/ios/files/FileFindTool.java
@@ -23,8 +23,29 @@ public class FileFindTool {
         return result;
     }
 
+    public static void findFileList(String filepath, List<String> fileNames) {
+        File dir = new File(filepath);
+        // 判断是否存在目录
+        if (!dir.exists() || !dir.isDirectory()) {
+            return;
+        }
+        String[] files = dir.list();// 读取目录下的所有目录文件信息
+        for (int i = 0; i < files.length; i++) {// 循环，添加文件名或回调自身
+            File file = new File(dir, files[i]);
+            if (file.isFile()) {// 如果文件
+                fileNames.add(dir + "\\" + file.getName());// 添加文件全路径名
+            } else {// 如果是目录
+                findFileList(file.getPath(), fileNames);// 回调自身继续查询
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Alog.i(getFiles("D:\\temp"));
+//        Alog.i(getFiles("D:\\temp\\ziptest\\解压后\\66666"));
+
+        List<String> files = new ArrayList<>();
+        findFileList("D:\\temp\\ziptest\\解压后\\66666", files);
+        Alog.i(files);
 //        Alog.i(DirFindTool.getPath("D:\\temp"));
     }
 }
---
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/ios/zips/ZipTool.java
@@ -0,0 +1,104 @@
+package com.yuzhyn.azylee.core.ios.zips;
+
+import com.yuzhyn.azylee.core.ios.dirs.DirTool;
+import com.yuzhyn.azylee.core.ios.files.FileTool;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class ZipTool {
+
+    /**
+     * 压缩（未测试）
+     *
+     * @return
+     * @throws IOException
+     */
+    public static boolean zip() throws IOException {
+        //压缩文件名称与位置
+        OutputStream outputStream = new FileOutputStream("f://test.zip");
+        //获取压缩文件输出流
+        ZipOutputStream out = new ZipOutputStream(outputStream);
+        //被压缩的文件
+        FileInputStream fis = new FileInputStream("F://video.mp4");
+        //video.mp4在压缩包zip 中的音乐名称
+        out.putNextEntry(new ZipEntry("视频.mp4"));
+        int len;
+        byte[] buffer = new byte[1024];
+        // 读入需要下载的文件的内容，打包到zip文件
+        while ((len = fis.read(buffer)) > 0) {
+            //写入压缩文件输出流中(压缩文件zip中)
+            out.write(buffer, 0, len);
+        }
+        fis.close();
+
+        //第二个被压缩文件
+        FileInputStream fis1 = new FileInputStream("F://picture.jpg");
+        //picture.jpg在压缩包中的名称 图片.jpg
+        out.putNextEntry(new ZipEntry("图片.jpg"));
+        int len1;
+        byte[] buffer1 = new byte[1024];
+        // 读入需要下载的文件的内容，打包到zip文件
+        while ((len1 = fis1.read(buffer1)) > 0) {
+            out.write(buffer1, 0, len1);
+        }
+        //关闭流
+        out.closeEntry();
+        fis1.close();
+        out.close();
+        return true;
+    }
+
+    /**
+     * 解压zip文件
+     *
+     * @param zipFilePath  输入的zip文件路径
+     * @param targetPath   输出的文件夹路径,不存在会则创建
+     * @param targetFolder 输出的文件名
+     * @throws IOException 当然报io错误咯
+     */
+    public static void unzip(String zipFilePath, String targetPath, String targetFolder) throws IOException {
+
+        String finalTargetPath = DirTool.combine(targetPath, targetFolder);
+        if (!DirTool.create(finalTargetPath)) throw new IOException("创建文件夹失败");
+        if (!FileTool.isExist(zipFilePath)) throw new IOException("未发现zip文件");
+
+        File file = new File(zipFilePath);
+        //用GBK能解决很多问题
+        ZipFile zipFile = new ZipFile(file, Charset.forName("GBK"));
+        Enumeration<?> zipE = zipFile.entries();
+        while (zipE.hasMoreElements()) {
+            ZipEntry zipEntry = (ZipEntry) zipE.nextElement();
+            String savePath = DirTool.combine(finalTargetPath, zipEntry.getName());
+            if (zipEntry.isDirectory()) {
+                DirTool.create(savePath);
+            } else {
+                File uFile = new File(savePath);
+                // 覆盖（先删除，再创建）
+                FileTool.delete(savePath);
+                uFile.createNewFile();
+
+                try (InputStream inputStream = zipFile.getInputStream(zipEntry);
+                     FileOutputStream ofs = new FileOutputStream(uFile)) {
+                    int length;
+                    byte[] bytes = new byte[1024];
+                    while ((length = inputStream.read(bytes, 0, bytes.length)) != -1) {
+                        ofs.write(bytes, 0, length);
+                    }
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        unzip("D:\\temp\\ziptest\\temp.zip", "D:\\temp\\ziptest\\解压后", "66666");
+
+    }
+}
