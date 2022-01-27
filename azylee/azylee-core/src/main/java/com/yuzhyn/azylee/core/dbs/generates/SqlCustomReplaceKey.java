4c1867c6aba5756cab1d8ffae0de8aa098605203
于正洋 <yuzhyn@163.com>
2022-01-27 13:56:27 +0800

    SQL替换字符完善

--- azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/dbs/generates/SqlCustomReplaceKey.java
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/dbs/generates/SqlCustomReplaceKey.java
@@ -11,6 +11,9 @@ public class SqlCustomReplaceKey {
         LocalDateTime now = LocalDateTime.now();
         s = StringTool.replace(s, "${{time.now.MM}}", DateTimeFormat.toStr(now, DateTimeFormatPattern.SHORT_MONTH));
         s = StringTool.replace(s, "${{time.now.yyyyMM}}", DateTimeFormat.toStr(now, DateTimeFormatPattern.SHORT_YEAR_MONTH));
+        s = StringTool.replace(s, "${{time.now-3day.yyyy-MM-dd}}", DateTimeFormat.toStr(now.plusDays(-3), DateTimeFormatPattern.NORMAL_DATE));
+        s = StringTool.replace(s, "${{time.now-7day.yyyy-MM-dd}}", DateTimeFormat.toStr(now.plusDays(-7), DateTimeFormatPattern.NORMAL_DATE));
+        s = StringTool.replace(s, "${{time.nowmon-1st.yyyy-MM-dd}}", DateTimeFormat.toStr(now, DateTimeFormatPattern.NORMAL_YEAR_MONTH) + "-01");
         s = StringTool.replace(s, "${{time.lstmon-1st.yyyy-MM-dd}}", DateTimeFormat.toStr(now.plusMonths(-1), DateTimeFormatPattern.NORMAL_YEAR_MONTH) + "-01");
         return s;
     }
