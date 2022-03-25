c42d064262e854b36069bb17c2329452eb161fb4
于正洋 <yuzhyn@163.com>
2022-03-25 19:37:52 +0800

    区域信息

--- azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/consts/areas/AreaModel.java
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/consts/areas/AreaModel.java
@@ -1,15 +1,45 @@
 package com.yuzhyn.azylee.core.datas.consts.areas;
 
 public class AreaModel {
+    /**
+     * id
+     */
     private Integer id;
+    /**
+     * 名称
+     */
     private String name;
+    /**
+     * 父id
+     */
     private Integer parentId;
+    /**
+     * 简称
+     */
     private String shortName;
+    /**
+     * 层级
+     */
     private Integer levelType;
+    /**
+     * 城市编码
+     */
     private String cityCode;
+    /**
+     * 邮政编码
+     */
     private String zipCode;
+    /**
+     * 经度
+     */
     private Double lng;
+    /**
+     * 纬度
+     */
     private Double lat;
+    /**
+     * 拼音
+     */
     private String pinyin;
 
     public AreaModel(Integer id, String name, Integer parentId, String shortName, Integer levelType, String cityCode, String zipCode, Double lng, Double lat, String pinyin) {
@@ -22,6 +52,86 @@ public class AreaModel {
         this.zipCode = zipCode;
         this.lng = lng;
         this.lat = lat;
+        this.pinyin = pinyin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public Integer getLevelType() {
+        return levelType;
+    }
+
+    public void setLevelType(Integer levelType) {
+        this.levelType = levelType;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
         this.pinyin = pinyin;
     }
 }
--- azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/consts/areas/AreaTool.java
+++ azylee/azylee-core/src/main/java/com/yuzhyn/azylee/core/datas/consts/areas/AreaTool.java
@@ -2,13 +2,28 @@ package com.yuzhyn.azylee.core.datas.consts.areas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AreaTool {
     private List<AreaModel> areaModelList = new ArrayList<>();
 
     public void create() {
         areaModelList.addAll(AreaCreater.create());
-        // 序列化为树结构
+    }
 
+    /**
+     * 获取省列表
+     * @return
+     */
+    public List<AreaModel> getProvince() {
+        return areaModelList.stream().filter(x -> x.getLevelType() == 1).collect(Collectors.toList());
+    }
+    /**
+     * 获取城市列表
+     * @return
+     */
+    public List<AreaModel> getProvince(String name) {
+        // 首先应该按照名称获取省信息，然后通过父id过滤出城市列表
+        return areaModelList.stream().filter(x -> x.getLevelType() == 2).collect(Collectors.toList());
     }
 }
